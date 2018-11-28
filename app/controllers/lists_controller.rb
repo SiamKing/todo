319ba6f098bddbaa982c63c4406243e15a13d385
SiamKing <thesiameseelectric@gmail.com>
2018-11-28 11:02:26 -0800

    Added "create" action and private list_params method

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,10 +2,24 @@ class ListsController < ApplicationController
 
     def index
         @lists = List.all
+        @list = List.new
         # raise @lists.inspect
     end
 
     def show
         @list = List.find_by_id(params[:id])
+    end
+
+    def create
+        @list = List.new(list_params)
+        @list.save
+        redirect_to list_path(@list)
+    end
+
+
+    private
+
+    def list_params
+        params.require(:list).permit(:name)
     end
 end
