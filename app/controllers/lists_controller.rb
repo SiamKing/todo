7db5b0fca832bf017e26ebc72e3f71d7ea79f3f8
SiamKing <thesiameseelectric@gmail.com>
2018-12-06 16:54:25 -0800

    Added conditional in create action to ensure list was saved without errors

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,14 +8,18 @@ class ListsController < ApplicationController
 
     def show
         @list = List.find_by_id(params[:id])
+        @item = @list.items.build
     end
 
     def create
         @list = List.new(list_params)
-        @list.save
-        redirect_to list_path(@list)
+        if @list.save
+            redirect_to list_path(@list)
+        else
+            @lists = List.all
+            render :index
+        end
     end
-
 
     private
 
