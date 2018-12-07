3dcef8a14ecef5f1df86b278c9bceb58f8d15298
SiamKing <thesiameseelectric@gmail.com>
2018-12-06 19:48:02 -0800

    Added instance methods to check if status is complete or not

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,16 @@ class Item < ApplicationRecord
     belongs_to :list, optional: true
 
     validates :description, presence: true
+    STATUS = {
+        incomplete: 0,
+        complete: 1
+    }
+
+    def complete?
+        status == STATUS[:complete]
+    end
+
+    def incomplete?
+        status == STATUS[:incomplete]
+    end
 end
