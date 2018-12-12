f1dec2809721beaf78197a72d2fca5dd59d3e16b
SiamKing <thesiameseelectric@gmail.com>
2018-12-11 19:17:51 -0800

    created users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_07_033743) do
+ActiveRecord::Schema.define(version: 2018_12_11_211246) do
 
   create_table "items", force: :cascade do |t|
     t.string "description"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 2018_12_07_033743) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
