bb58ecdc8a917448de33a9cdcf66c8bd3b3eeb81
SiamKing <thesiameseelectric@gmail.com>
2018-12-11 19:04:38 -0800

    Added methods for authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+
+    def authentication_required
+        redirect_to login_path unless logged_in?
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    def current_user
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+
+    helper_method :current_user
 end
