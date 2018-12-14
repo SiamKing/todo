1abcafeaf55a89d861ee70a73a7855235330dcbd
SiamKing <thesiameseelectric@gmail.com>
2018-12-13 16:57:50 -0800

    Added test for item uniqueness validation

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Item, type: :model do
     Item.destroy_all
     create_valid_user('foo@bar.com', 'foobar33')
     @list = List.create!(name: "Shopping List", user_id: @valid_user.id)
-    @item = @list.items.create!(description: "Creamer", list_id: 1)
+    @item = @list.items.create!(description: "Creamer")
   end
 
   it "can be created with valid attributes" do
@@ -15,7 +15,14 @@ RSpec.describe Item, type: :model do
 
   it "is invalid without a description" do
     @invalid_item = Item.new
+
     expect(@invalid_item).to be_invalid
+  end
+
+  it "is invalid if it is already on list" do
+    @non_unique_item = @list.items.new(description: "Creamer")
+
+    expect(@non_unique_item).to be_invalid
   end
 
   it "can create list association" do
@@ -23,6 +30,7 @@ RSpec.describe Item, type: :model do
     @item1 = Item.new(description: "Build out list functionality")
     @item1.list = @list1
     @item1.save!
+    
     expect(@item1.list).to eq(@list1)
   end
 
