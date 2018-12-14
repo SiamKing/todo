f54872edeaf8c47c4cbd6940c099902e333f3150
SiamKing <thesiameseelectric@gmail.com>
2018-12-13 16:57:21 -0800

    Added new columns to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_12_034606) do
+ActiveRecord::Schema.define(version: 2018_12_13_235956) do
 
   create_table "items", force: :cascade do |t|
     t.string "description"
@@ -33,6 +33,9 @@ ActiveRecord::Schema.define(version: 2018_12_12_034606) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "uid"
+    t.string "name"
+    t.string "image"
   end
 
 end
